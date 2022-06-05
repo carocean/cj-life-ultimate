f2f81ed756b88996399902fde27575bd4313352a
caroceanjofers <carocean.jofers@icloud.com>
2022-06-06 01:10:00 +0800

    0.0.1 cloud-2021.2

--- cj-life-ability-oauth2-server/src/main/java/cj/life/ability/oauth2/DefaultSuccessAuthentication.java
+++ cj-life-ability-oauth2-server/src/main/java/cj/life/ability/oauth2/DefaultSuccessAuthentication.java
@@ -7,11 +7,14 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: cj
@@ -25,7 +28,29 @@ public class DefaultSuccessAuthentication extends SavedRequestAwareAuthenticatio
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         ResultCode rc = ResultCode.IS_AUTHORIZED;
-        Object obj = R.of(rc, request.getQueryString());
+        String response_type = request.getParameter("response_type");
+        String client_id = request.getParameter("client_id");
+        String grant_type = request.getParameter("grant_type");
+        String redirect_uri = request.getParameter("redirect_uri");
+        String scope = request.getParameter("scope");
+        Map<String, String> map = new HashMap<>();
+        if (StringUtils.hasText(response_type)) {
+            map.put("response_type", response_type);
+        }
+        if (StringUtils.hasText(grant_type)) {
+            map.put("grant_type", grant_type);
+        }
+        if (StringUtils.hasText(client_id)) {
+            map.put("client_id", client_id);
+        }
+        if (StringUtils.hasText(redirect_uri)) {
+            map.put("redirect_uri", redirect_uri);
+        }
+        if (StringUtils.hasText(scope)) {
+            map.put("scope", scope);
+        }
+
+        Object obj = R.of(rc, map);
         response.getWriter().write(new ObjectMapper().writeValueAsString(obj));
     }
 }
