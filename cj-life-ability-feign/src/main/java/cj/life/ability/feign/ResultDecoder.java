5efb5c298b91cccc84c95887ad82e94a1d8b966a
cj <cj@ss.com>
2022-07-27 19:35:23 +0800

    0.0.1 cloud-2021.2

--- cj-life-ability-feign/src/main/java/cj/life/ability/feign/ResultDecoder.java
+++ cj-life-ability-feign/src/main/java/cj/life/ability/feign/ResultDecoder.java
@@ -1,6 +1,8 @@
 package cj.life.ability.feign;
 
 import cj.life.ability.api.R;
+import cj.life.ability.api.ResultCode;
+import cj.life.ability.api.exception.ApiException;
 import feign.FeignException;
 import feign.Response;
 import feign.codec.Decoder;
@@ -12,16 +14,16 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 
 /**
- * @author changjin wei(魏昌进)
+ * @author andrew jofers
  * @since 2022/4/8
  */
 public class ResultDecoder implements Decoder {
     private Decoder decoder;
- 
+
     public ResultDecoder(Decoder decoder) {
         this.decoder = decoder;
     }
- 
+
     @Override
     public Object decode(Response response, Type type) throws IOException, FeignException {
         Method method = response.request().requestTemplate().methodMetadata().method();
@@ -29,6 +31,9 @@ public class ResultDecoder implements Decoder {
         if (isResult) {
             ParameterizedTypeImpl resultType = ParameterizedTypeImpl.make(R.class, new Type[]{type}, null);
             R<?> result = (R<?>) this.decoder.decode(response, resultType);
+            if (!ResultCode.SUCCESS.code().equals(result.getCode())) {
+                throw new ApiException(result.getCode(), result.getMessage());
+            }
             return result.getData();
         }
         return this.decoder.decode(response, type);
